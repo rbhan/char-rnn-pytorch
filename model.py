Updates, more args, fixed softmax

model.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 from torch.autograd import Variable
-import torch.nn.functional as F
 
 
 class CharRNNModel(nn.Module):
@@ -10,16 +9,25 @@ class CharRNNModel(nn.Module):
         # Parameters
         self.num_layers = config.num_layers
         self.rnn_size = config.rnn_size
+        self.rnn_model = config.rnn_model
 
         # Layers (containing weights)
         self.embedding = nn.Embedding(
             config.vocab_size, self.rnn_size,
         )
-        self.rnn = nn.LSTM(
+
+        if self.rnn_model == "LSTM":
+            rnn_cell = nn.LSTM
+        elif self.rnn_model == "GRU":
+            rnn_cell = nn.GRU
+        else:
+            raise KeyError()
+
+        self.rnn = rnn_cell(
             self.rnn_size,
             self.rnn_size,
             self.num_layers,
-            dropout=(1-config.input_keep_prob),
+            dropout=(1 - config.keep_prob),
         )
         self.fc1 = nn.Linear(self.rnn_size, config.vocab_size)
 
@@ -29,8 +37,6 @@ class CharRNNModel(nn.Module):
             [batch_size, seq_length, embedding_dim]
         However, PyTorch RNN cells work with the following convention:
             [seq_length, batch_size, embedding_dim]
-
-
 
         Because of that, as well as other PyTorch-specific dimension
         requirements, I'll do some dimension-hacking within the model.
@@ -53,16 +59,9 @@ class CharRNNModel(nn.Module):
             lstm_out.view(lstm_out.size(0) * lstm_out.size(1), lstm_out.size(2))
         ).view(lstm_out.size(0), lstm_out.size(1), self.fc1.out_features)
 
-        # Apply Log-Softmax. We temporarily switch the embedding dimension
-        # first:
-        #    [num_chars, seq_length, batch_size]
-        # This is because Softmax normalizes over the possible characters,
-        # so it needs to be the first dimension.
-        log_softmax_output = \
-            F.log_softmax(output.permute(2, 1, 0)).permute(2, 1, 0)
-
+        # Switch to back to [batch_size, seq_length, embedding_dim]
         return (
-            log_softmax_output.permute(1, 0, 2),
+            output.permute(1, 0, 2),
             hidden,
         )
 
@@ -70,11 +69,16 @@ class CharRNNModel(nn.Module):
         """Initialize hidden weights"""
         # LSTMs require two sets of hidden states
         weight = next(self.parameters()).data
-        return (
-            Variable(
-                weight.new(self.num_layers, batch_size, self.rnn_size).zero_()
-            ),
-            Variable(
+
+        h = Variable(
+            weight.new(self.num_layers, batch_size, self.rnn_size).zero_()
+        )
+        if self.rnn_model == "LSTM":
+            c = Variable(
                 weight.new(self.num_layers, batch_size, self.rnn_size).zero_()
             )
-        )
+            return h, c
+        elif self.rnn_model == "GRU":
+            return h
+        else:
+            raise KeyError(self.rnn_model)

sample.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
-from utils import new_hidden, get_sampling_function, to_text
+from utils import new_hidden, get_sampling_function, to_text, maybe_cuda_var
 
 
 def sample_model(config, dataset, model,
@@ -22,8 +22,10 @@ def sample_model(config, dataset, model,
     for i in range(length):
         # Only use the last runner.seq_length for prediction input
         pred_input = history[:, -config.seq_length:]
+        pred_input_var = maybe_cuda_var(
+            torch.LongTensor(pred_input), cuda=config.cuda)
         output_gen, hidden_gen = model(
-            Variable(torch.LongTensor(pred_input)).cuda(),
+            pred_input_var,
             hidden_gen,
         )
 

train.py
@@ -1,16 +1,19 @@
-import numpy as np
+from __future__ import print_function
+import os
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.utils
 
 from model import CharRNNModel
 from data import load_data
-from utils import new_hidden, repackage_sample
+from utils import new_hidden, maybe_cuda_var, set_seed
 from sample import sample_model
 
 
-def train_model(config,
+def train_model(config, verbose=True, seed=1,
                 sample_prime_text="The ", sample_length=50):
+
+    set_seed(config, seed=seed)
     dataset, dataloader = load_data(config)
 
     model = CharRNNModel(config)
@@ -27,14 +30,14 @@ def train_model(config,
 
     # Training Loop
     model.train()
-    print "START TRAINING for {} Epochs".format(config.num_epochs)
+    print("START TRAINING for {} Epochs".format(config.num_epochs))
     for epoch in range(config.num_epochs):
         batch_loss = 0
         for x_batch, y_batch in dataloader:
 
             # Pre-process inputs
-            x_var = repackage_sample(x_batch, config=config)
-            y_var = repackage_sample(y_batch, config=config)
+            x_var = maybe_cuda_var(x_batch, cuda=config.cuda)
+            y_var = maybe_cuda_var(y_batch, cuda=config.cuda)
 
             # Run, perform gradient-descent
             model.zero_grad()
@@ -56,23 +59,39 @@ def train_model(config,
         losses.append(batch_loss)
 
         # Print status
-        print "Epoch={}/{}: Loss={}".format(
+        maybe_print("Epoch={}/{}: Loss={}".format(
             epoch, config.num_epochs, batch_loss,
-        )
-        print "Sample: "
-        print indent_text(sample_model(
-            config=config,
-            dataset=dataset,
-            model=model,
-            prime_text=sample_prime_text,
-            length=sample_length,
-        ))
+        ), verbose=verbose)
+
+        if verbose:
+            print("Sample: ")
+            print(indent_text(sample_model(
+                config=config,
+                dataset=dataset,
+                model=model,
+                prime_text=sample_prime_text,
+                length=sample_length,
+            )))
+
+        if config.save_every and epoch % config.save_every == 0:
+            save_path = os.path.join(
+                config.save_dir,
+                "{:04d}.ckpt".format(epoch)
+            ),
+            maybe_print("Saving to ".format(save_path), verbose=verbose)
+            torch.save(model, save_path)
 
         # Decay Learning Rate
         for param_group in optimizer.param_groups:
             param_group['lr'] *= config.decay_rate
 
+    print("DONE TRAINING for {} Epochs".format(config.num_epochs))
     return dataset, model, losses
+
+
+def maybe_print(text, verbose):
+    if verbose:
+        print(text)
 
 
 def indent_text(text, indentation=8):

utils.py
@@ -1,5 +1,15 @@
 import numpy as np
+import os
+
+import torch
 from torch.autograd import Variable
+
+
+def get_data_dir(dataset_name=""):
+    """Get path to data directory"""
+    return os.path.realpath(
+        os.path.join(__file__, "..", "data", dataset_name)
+    )
 
 
 def to_text(x, dataset):
@@ -8,22 +18,21 @@ def to_text(x, dataset):
 
 
 def new_hidden(model, batch_size):
+    """Get a new hidden variable"""
     return repackage_hidden(model.init_hidden(batch_size))
 
 
-def repackage_sample(x, config):
+def maybe_cuda_var(x, cuda):
     """Helper for converting to a Variable"""
     x = Variable(x)
-    if config.cuda:
+    if cuda:
         x = x.cuda()
     return x
 
 
 def repackage_hidden(h):
-    """
-    Repackages a hidden state as a new variable. The stops gradients from
-    flowing back further.
-    """
+    """Repackages a hidden state as a new variable. The stops gradients
+    from flowing back further."""
     if type(h) == Variable:
         return Variable(h.data)
     else:
@@ -31,6 +40,7 @@ def repackage_hidden(h):
 
 
 def get_sampling_function(config):
+    """Get function for sampling a character"""
     if config.sampling_mode == "argmax":
         return np.argmax
     elif config.sampling_mode == "weighted":
@@ -44,3 +54,11 @@ def weighted_pick(weights):
     t = np.cumsum(weights)
     s = np.sum(weights)
     return int(np.searchsorted(t, np.random.rand(1)*s))
+
+
+def set_seed(config, seed=1):
+    """Set seed for Torch, CUDA and NumPy"""
+    torch.manual_seed(seed)
+    if config.cuda:
+        torch.cuda.manual_seed(seed)
+    np.random.seed(seed)